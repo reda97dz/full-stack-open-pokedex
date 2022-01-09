f2c5ae2c12b1913acd95426cc3ae2151cdc9d402
Reda Baka <17656625+reda97dz@users.noreply.github.com>
2022-01-09 14:41:21 +0100

    fix lint errors

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,18 +1,18 @@
-describe("Pokedex", function () {
-  it("front page can be opened", function () {
-    cy.visit("http://localhost:8080");
-    cy.contains("ivysaur");
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur')
     cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
-    );
-  });
-});
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
+})
 
-describe("Pokedex", function () {
-  it("pokemon page can be opened", function () {
-    cy.visit("http://localhost:8080");
-    cy.contains("ivysaur").click();
-    cy.contains("ivysaur");
-    cy.contains("Chlorophyll");
-  });
-});
+describe('Pokedex', function () {
+  it('pokemon page can be opened', function () {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('Chlorophyll')
+  })
+})
